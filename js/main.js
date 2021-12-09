a88aaeec7ea1203fd50db2ebc7087fe894f98113
iamigrek <gerevyanko.igorek@gmail.com>
2021-12-09 03:30:03 +0200

    Исправил баг в Projects

--- js/main.js
+++ js/main.js
@@ -429,6 +429,22 @@ function navigation() {
   });
 }
 
+const project = document.querySelectorAll('.project');
+let projectActive;
+
+project.forEach(item => {
+  item.addEventListener('click', () => {
+    project.forEach(el => el.classList.remove('project--active'));
+    if (item == projectActive) {
+      item.classList.remove('project--active');
+      projectActive = '';
+    } else {
+      item.classList.add('project--active');
+      projectActive = item;
+    }
+  });
+});
+
 const projectsItem = document.querySelectorAll('.projects__item');
 
 projectsItem.forEach(item => {
